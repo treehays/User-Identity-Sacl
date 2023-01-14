e5c1a2c6924f540b372fa79693a370c8f4d6c3fb
ABDULSALAM Ahmad Ayoola <33053528+treehays@users.noreply.github.com>
2023-01-14 09:03:44 +0100

    Added Service to wire user

--- CustomerScaffold/Program.cs
+++ CustomerScaffold/Program.cs
@@ -1,4 +1,5 @@
 using CustomerScaffold.Data;
+using CustomerScaffold.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+/// Adding aditional service this shows how users is been wired
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultUI()
+            .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
